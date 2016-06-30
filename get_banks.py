1bd3ec5e2c741d38415934d8be1ff4c5a86bec4e
AnastasiaChetrusca <achetrusca@yahoo.com>
2016-06-30 16:18:55 +0300

    first commit

--- get_banks.py
+++ get_banks.py
@@ -1,26 +1,43 @@
 from bs4 import BeautifulSoup
 import urllib
 
-def get_banks():
+def get_banks(index):
 
 	page = urllib.urlopen('http://index.minfin.com.ua/bank/?mfo').read()
 	soup = BeautifulSoup(page, 'html.parser')
-	tables = soup.select("table ")
+
+	tables = soup.select("table")[index]
 	result = {}
-	for table in tables:
-		rows = table.select("tr")
-		for row in rows:
-			entries = row.select("td")
-			if entries:
-				result[entries[0].text] = ' BANK and City: '+ entries[1].text  #=' BANK: '+entries[1].text + ' CITY: '+entries[2].text
+	rows = tables.select("tr")
+
+	for row in rows:
+		entries = row.select("td")
+		
+		if entries:
+
+			key = entries[0].text
+			bank = entries[1].text
+			city = entries[2].text
+
+			result[key]=('BANK: '+bank, 'CITY: '+city )
 	
 	return result
 
 
  
 if __name__=="__main__":
-	a = get_banks()
-	type(a)
-	for key in a:
-		print key, a[key]
+
+	for i in range (0,3):
+
+		if i == 0:
+			print "\n\n\nExisting banks :"
+		if i == 1:
+			print "\n\n\nLiquidated banks :"
+		if i == 2:
+			print "\n\n\nPrivate banks :"		
+
+		a = get_banks(i)
+		type(a)
+		for key, value in a.items():
+			print key ,value[0], value[1]
 
